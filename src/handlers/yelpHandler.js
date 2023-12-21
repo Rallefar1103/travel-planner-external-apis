fc16a4b54ba0f8e8a741c71c48a9cb2d0448ff3f
Rallefar1103 <rasmus@turftank.com>
2023-12-20 19:57:21 -0800

    sets up the yelp api

--- src/handlers/yelpHandler.js
+++ src/handlers/yelpHandler.js
@@ -1,2 +1,13 @@
 const axios = require("axios");
 require("dotenv").config();
+
+async function fetchYelpData(client, searchParams) {
+  try {
+    const response = await client.search(searchParams);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = fetchYelpData;
--- src/routes/yelpRoutes.js
+++ src/routes/yelpRoutes.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
+const yelp = require("yelp-fusion");
+const { fetchYelpData } = require("../handlers/yelpHandler");
+
 require("dotenv").config();
 
-router.post("/getYelpData", async (req, res) => {});
+const baseURL = "https://api.yelp.com/v3";
+const client = yelp.client(process.env.YELP_API_KEY);
+
+router.post("/getYelpData", async (req, res) => {
+  const { term, location } = req.body;
+
+  const searchRequest = {
+    term: term,
+    location: location,
+  };
+
+  try {
+    const data = await fetchYelpData(client, searchRequest);
+
+    res.json(data);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = router;
